4ddf75c1d875264cb943773a2438b5331d34473c
damianmediavilla <damianmediavilla@hotmail.com>
2023-07-15 12:36:54 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+  </nav>
+  <nav class="navbar navbar-expand navbar-light bg-light">
+    <div class="nav navbar-nav">
+        <router-link class="nav-item nav-link" to="/crear">Crear</router-link>
+        <router-link class="nav-item nav-link" to="/listar">Listar</router-link>
+      </div>
+  </nav>
+  <br>
+  <router-view/>
+</template>
+
+
